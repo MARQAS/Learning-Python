2fff6f231904773fd2734633ffe0b06e841a93fb
Mohammed Razzaq <marqas36@gmail.com>
2017-11-06 02:52:16 +0530

    #1 Dictionary

--- python-II.py
+++ python-II.py
@@ -1,5 +1,5 @@
 #Lists
-#list of (strings/numbers) = array
+#list of (strings/numbers) = array (without key value pair)
 performances = ['Ventriloquism', 'Amazing Acrobatics']
 # New items can be added in the list using append()
 performances.append('Snake Charmer')
@@ -14,3 +14,15 @@ print(performances)
 # del is used when index is know but value is unknown
 del performances[1]
 print(performances)
+
+# Dictionaries = arrays (with key=>value pair)
+# Dictionary = {'key':'value'}
+performances = {'Ventriloquism':'9:00am', 'Snake Charmer':'12:00pm'}
+# Add new items
+performances['Amazing Acrobatics'] = '2:00pm'
+performances['Enchanted Elephants'] = '5:00pm'
+print(performances)
+# update the existing value
+performances['Enchanted Elephants'] = '6:00pm'
+# items can be deleted using del
+del performances['Ventriloquism']
