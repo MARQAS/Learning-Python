fc0a25f87b13ff58d0cbef1c510f927967d02204
Mohammed Razzaq <marqas36@gmail.com>
2017-11-05 19:02:52 +0530

    #1 List with append example

---
+++ python-II.py
@@ -0,0 +1,6 @@
+#Lists
+#list of (strings/numbers) = array
+performances = ['Ventriloquism', 'Amazing Acrobatics']
+# New items can be added in the list using append()
+performances.append('Snake Charmer')
+print(performances)
