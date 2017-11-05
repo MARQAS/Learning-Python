94dcd264a22fb39eef4ef083695ef6839733e8b3
Mohammed Razzaq <marqas36@gmail.com>
2017-11-05 19:09:24 +0530

    #1 Remove list items using remove()

--- python-II.py
+++ python-II.py
@@ -3,4 +3,11 @@
 performances = ['Ventriloquism', 'Amazing Acrobatics']
 # New items can be added in the list using append()
 performances.append('Snake Charmer')
+performances.append('Bearded Lady')
+performances.append('Tiniest Man')
+print(performances)
+# Items can be removed using remove() and del
+# remove()
+performances.remove('Bearded Lady')
+performances.remove('Tiniest Man')
 print(performances)
